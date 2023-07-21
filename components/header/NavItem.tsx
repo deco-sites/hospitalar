148041a24e73ef3161335202eff7004a01df540f
Mateus Martins Pinheiro <mateusmartinsmck@gmail.com>
2023-07-20 21:56:35 -0300

    feat: menu e dropdown menu refeito segundo layout

--- components/header/NavItem.tsx
+++ components/header/NavItem.tsx
@@ -1,5 +1,6 @@
 import { headerHeight } from "./constants.ts";
 import Icon from "$store/components/ui/Icon.tsx";
+import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 
 export interface INavItem {
   label: string;
@@ -7,7 +8,19 @@ export interface INavItem {
   highlighted?: boolean;
   children?: INavItem[];
   variant?: "CommonChild" | "AllCategories" | "WithBrands" | "Other";
+  image?: Image;
 }
+
+export interface Image {
+  src?: LiveImage;
+  alt?: string;
+  href?: string;
+}
+
+const colsVariants: Record<string, string> = {
+  "Marcas": "grid-cols-3",
+  "Medicamentos": "grid-cols-11",
+};
 
 function splitNatItems(children: INavItem[], number = 6) {
   const slices = [];
@@ -20,13 +33,43 @@ function splitNatItems(children: INavItem[], number = 6) {
   return slices;
 }
 
-function NavItemDropDown({ elements }: { elements?: INavItem[] }) {
+function NavItemDropDown({ elements, variant, image }: { elements?: INavItem[], variant?: string, image?:Image }) {
   if (!elements || !elements?.length) {
     return <span />;
   }
-
+  if (variant === "WithBrands") return (
+    <div
+      class="absolute hidden hover:flex group-hover:flex bg-base-100 z-50 items-start justify-center gap-6 w-screen shadow-md"
+      style={{ top: "0px", left: "0px", marginTop: headerHeight }}
+    >
+      <div class="container w-full pt-5 pb-5 m-auto px-5 flex items-start justify-start gap-16">
+        {elements.map((element) => { 
+          return(
+          <div class="mr-[83px]">
+            <span>{element.label}</span>
+            <ul class={`mt-3 grid ${colsVariants[element.label]} gap-x-[14px]`}>
+              {element.children && element.children.map((child) => (
+                <li class="mb-3">
+                  <a
+                    class="text-sm text-base-content hover:font-bold hover:underline transition-all duration-300"
+                    href={child.href || ""}
+                  >
+                    <span>{child.label}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )})}
+        {image && (
+          <a href={image.href || ''}>
+            <img src={image.src} alt={image.alt || "Banner vertical do menu"} class="h-full w-auto justify-self-end" />
+          </a>
+        )}
+      </div>
+    </div>
+  )
   const navItemsCol = splitNatItems(elements, 16);
-
   return (
     <div
       class="absolute hidden hover:flex group-hover:flex bg-base-100 z-50 items-start justify-center gap-6 w-screen shadow-md"
@@ -53,8 +96,7 @@ function NavItemDropDown({ elements }: { elements?: INavItem[] }) {
 }
 
 function NavItem({ item }: { item: INavItem }) {
-  const { href, label, children, highlighted } = item;
-
+  const { href, label, children, highlighted, variant, image } = item;
   return (
     <li
       class={`group flex items-center ${
@@ -80,7 +122,7 @@ function NavItem({ item }: { item: INavItem }) {
           {label}
         </span>
       </a>
-      <NavItemDropDown elements={children} />
+      <NavItemDropDown variant={variant} elements={children} image={image} />
     </li>
   );
 }
