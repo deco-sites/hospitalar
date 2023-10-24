4bd76b8b4dae0d7a972a57505a15207a3e8f0997
Lucas <carvalho1104@hotmail.com>
2023-10-24 19:12:26 -0300

    fix: task/9959 - fixed the background color bug of the promotion tag.

--- components/product/DiscountBadge.tsx
+++ components/product/DiscountBadge.tsx
@@ -1,22 +1,44 @@
+// deno-lint-ignore-file
 type Props = {
   price: number;
   listPrice: number;
   label?: string;
-  variant?: string;
+  variant?: keyof typeof bgColor;
   className?: string;
 };
 
-function DiscountBadge({ price, listPrice, label, variant, className }: Props) {
+const bgColor = {
+  emphasis: "bg-emphasis",
+  primary: "bg-primary",
+  secondary: "bg-secondary",
+  neutral: "bg-neutral",
+  accent: "bg-accent",
+  success: "bg-success",
+  info: "bg-info",
+  error: "bg-error",
+  warning: "bg-warning",
+  undefined: "bg-emphasis",
+};
+
+function DiscountBadge({ price, listPrice, label, variant= 'undefined', className }: Props) {
   const discount = ((listPrice - price) / listPrice) * 100;
+
+  let color = "";
+
+  let index = variant;
+
+  if (variant) {
+    color = bgColor[index];
+  }
 
   return (
     <div
       class={`absolute left-0 top-0 p-[10px] flex items-center z-10 ${className}`}
     >
       <div
-        class={`text-xs uppercase font-bold border-none px-[10px] py-[7px] rounded-lg flex box-content bg-opacity-100 opacity-100 text-base-100 bg-${
-          variant ?? "emphasis"
-        }`}
+        class={`text-xs uppercase font-bold border-none px-[10px] py-[7px] rounded-lg flex box-content bg-opacity-100 opacity-100 text-base-100 ${
+          color && `${color}`
+        } `}
       >
         {discount?.toFixed(2).slice(0, 2)}% {label ?? "OFF"}
       </div>
