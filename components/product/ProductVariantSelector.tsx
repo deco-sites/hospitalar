d0e8a26f07bf8adc76b80214ff644af7f96f60db
Marcus <marcus.castro@agencian1.com.br>
2025-04-15 11:48:28 -0300

    ajuste11064 - fix:ajuste flag

--- components/product/ProductVariantSelector.tsx
+++ components/product/ProductVariantSelector.tsx
@@ -1,41 +1,45 @@
 import Avatar from "$store/components/ui/Avatar.tsx";
-import { useVariantPossibilities } from "$store/sdk/useVariantPossiblities.ts";
-import type { Product } from "apps/commerce/types.ts";
-
-interface Props {
-  product: Product;
-  currentURL: string;
-}
-
-function VariantSelector({ product, currentURL }: Props) {
-  const possibilities = useVariantPossibilities(product);
-
-  return (
-    <ul class="flex flex-col gap-5">
-      {Object.keys(possibilities).map((name) => (
-        <li class="flex flex-col gap-[10px]">
-          <span class="font-poppins not-italic font-normal text-xs text-[#8E8E9F]">
-            {name}
-          </span>
-          <ul class="flex flex-row flex-wrap gap-[5px]">
-            {Object.entries(possibilities[name]).map((
-              [value, { urls, inStock }],
-            ) => (
-              <li>
-                <a href={urls[0]}>
-                  <Avatar
-                    content={value}
-                    variant={inStock ? "default" : "disabled"}
-                    active={urls[0] === currentURL}
-                  />
-                </a>
-              </li>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </ul>
-  );
-}
+ import { useVariantPossibilities } from "$store/sdk/useVariantPossiblities.ts";
+ import type { Product } from "apps/commerce/types.ts";
+ 
+ interface Props {
+   product: Product;
+   currentURL: string;
+ }
+ 
+ function VariantSelector({ product, product: { url }, currentURL }: Props) {
+   const possibilities = useVariantPossibilities(product);
+ 
+   console.log(Object.entries(possibilities))
+ 
+   return (
+     <ul class="flex flex-col gap-5">
+       {Object.entries(possibilities).map(([name, values]) => {
+         // Se houver apenas uma chave dentro de `values`, não renderizar
+         if (Object.keys(values).length <= 1) return null;
+         if (Object.keys(values).length <= 1 && name !== "Opção") return null;
+ 
+         return (
+           <li class="flex flex-col gap-[10px]" key={name}>
+             <span class="text-xs text-base-300">{name}</span>
+             <ul class="flex flex-row flex-wrap gap-[5px]">
+               {Object.entries(values).map(([value, { urls, inStock }]) => (
+                 <li key={value}>
+                   <a href={urls[0]}>
+                     <Avatar
+                       content={value}
+                       variant={inStock ? "default" : "disabled"}
+                       active={urls[0] === currentURL}
+                     />
+                   </a>
+                 </li>
+               ))}
+             </ul>
+           </li>
+         );
+       })}
+     </ul>
+   );
+ }
 
 export default VariantSelector;
